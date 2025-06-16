dd3da0b559b87c0a23c351422314b0feae0f93ba
Nick Yoder <nicholas.yoder@txse.com>
2025-06-16 12:12:31 -0700

    Cleaned up 2024/10/b.

--- AdventOfCode/2024/10/b.py
+++ AdventOfCode/2024/10/b.py
@@ -10,22 +10,22 @@ def count_paths(data):
     for y, line in enumerate(data):
         for x, height in enumerate(line):
             if height == 0:
-                valid_paths = [0]
-                count_paths_helper(data, y, x, valid_paths)
-                trail_heads.append(valid_paths[0])
+                trail_heads.append(count_paths_helper(data, y, x))
     return trail_heads
 
-def count_paths_helper(data, y, x, valid_paths):
+def count_paths_helper(data, y, x):
     if data[y][x] == 9:
-        valid_paths[0] += 1
-    if y - 1 != None and y - 1 >= 0 and data[y-1][x] == data[y][x] + 1:
-        count_paths_helper(data, y-1, x, valid_paths)
-    if y + 1 != None and y + 1 < len(data) and data[y+1][x] == data[y][x] + 1:
-        count_paths_helper(data, y+1, x, valid_paths)
-    if x - 1 != None and x - 1 >= 0 and data[y][x-1] == data[y][x] + 1:
-        count_paths_helper(data, y, x-1, valid_paths)
-    if x + 1 != None and x + 1 < len(data[y]) and data[y][x+1] == data[y][x] + 1:
-        count_paths_helper(data, y, x+1, valid_paths)
+        return 1
+    total = 0
+    if y - 1 >= 0 and data[y-1][x] == data[y][x] + 1:
+        total += count_paths_helper(data, y-1, x)
+    if y + 1 < len(data) and data[y+1][x] == data[y][x] + 1:
+        total += count_paths_helper(data, y+1, x)
+    if x - 1 >= 0 and data[y][x-1] == data[y][x] + 1:
+        total += count_paths_helper(data, y, x-1)
+    if x + 1 < len(data[y]) and data[y][x+1] == data[y][x] + 1:
+        total += count_paths_helper(data, y, x+1)
+    return total
 
 if __name__ == '__main__':
     data = load_data('input')
