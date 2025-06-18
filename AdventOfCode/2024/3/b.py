e3bb956c28b958e3bf966394d512434b92abbbd2
Nick Yoder <nicholas.yoder@txse.com>
2025-06-18 14:17:32 -0700

    Cleaned up 2024/3/b

--- AdventOfCode/2024/3/b.py
+++ AdventOfCode/2024/3/b.py
@@ -11,17 +11,13 @@ match = re.findall(pattern, text)
 pattern = r"\d+"
 sum = 0
 do_flag = True
-if match:
-    for m in match:
-        if m == "do()":
-            do_flag = True
-            continue
-        elif m == "don't()":
-            do_flag = False
-            continue
-        elif m.startswith("mul(") and do_flag:
-            m = m[4:-1]
-            a = [int(x) for x in m.split(",")]
-            sum += a[0] * a[1]
-
+for m in match:
+    if m == "do()":
+        do_flag = True
+    elif m == "don't()":
+        do_flag = False
+    elif do_flag:
+        m = m[4:-1]
+        a = [int(x) for x in m.split(",")]
+        sum += a[0] * a[1]
 print(sum)
