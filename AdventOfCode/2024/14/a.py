83e9d25c7156f17ec7896c887b6b5b85a09d6c10
Nick Yoder <nicholas.yoder@txse.com>
2025-06-18 11:36:02 -0700

    2024/14/a completed

---
+++ AdventOfCode/2024/14/a.py
@@ -0,0 +1,71 @@
+import re
+import os
+import time
+from functools import reduce
+import operator
+
+MAXX, MAXY = 101, 103
+
+def load_data(input):
+    robots = []
+    with open(input, 'r') as file:
+        lines = [line.strip() for line in file.readlines() if line.strip()]
+        for line in lines:
+            robots.append(list(map(int, re.findall(r'-?\d+', line))))
+    return robots
+
+def simulate_robot(robot: list, seconds, visualize=False):
+    x, y = robot[0], robot[1]
+    dx, dy = robot[2], robot[3]
+    while(seconds > 0):
+        new_x, new_y = x+dx, y+dy
+
+        if new_x < 0: new_x = MAXX + new_x
+        if new_x >= MAXX: new_x = new_x - MAXX
+        if new_y < 0: new_y = MAXY + new_y
+        if new_y >= MAXY: new_y = new_y - MAXY
+        x, y = new_x, new_y
+        seconds -= 1
+        if visualize: draw_robot_path((x,y))
+    return (x, y)
+
+def get_quadrants(end_positions: dict):
+    quad1 = quad2 = quad3 = quad4 = 0
+    for end_pos in end_positions.keys():
+        x, y = end_pos
+        if y < MAXY // 2:
+            if x < MAXX // 2:
+                quad1 += end_positions[end_pos]
+            elif x > MAXX // 2 - 1 + MAXX % 2:
+                quad2 += end_positions[end_pos]
+        elif y > MAXY // 2 - 1 + MAXY % 2:
+            if x < MAXX // 2:
+                quad3 += end_positions[end_pos]
+            elif x > MAXX // 2 - 1 + MAXX % 2:
+                quad4 += end_positions[end_pos]
+    return (quad1, quad2, quad3, quad4)
+
+def draw_robot_path(coords):
+    rows = []
+    for i in range(MAXY):
+        row = []
+        for j in range(MAXX):
+            if j == coords[0] and i == coords[1]:
+                row.append('#')
+            else:
+                row.append('.')
+        rows.append(' '.join(row))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\n'.join(rows))
+    time.sleep(0.1)
+    
+if __name__ == '__main__':
+    robots = load_data('input')
+    end_positions = {}
+    seconds = 100
+    for robot in robots:
+        end_pos = simulate_robot(robot, seconds)
+        end_positions[end_pos] = end_positions.get(end_pos, 0) + 1
+    quads = get_quadrants(end_positions)
+    print(f"Quadrants = {quads}")
+    print(f"Safety factor of {reduce(operator.mul, quads, 1)}")
