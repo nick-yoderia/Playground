ed74226db552fda463a3135677f98e53f96ca593
Nick Yoder <nicholas.yoder@txse.com>
2025-07-07 09:23:59 -0700

    2024/18/b completed

---
+++ AdventOfCode/2024/18/b.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+import heapq
+import math
+import os
+
+def load_data(input):
+    with open(input, 'r') as file:
+        data = file.readlines()
+        coords = []
+        for line in data:
+            items = line.strip().split(',')
+            if len(items) == 2:
+                coords.append((int(items[0]), int(items[1])))
+        return coords
+    
+def simulate(num, coords):
+    corrupted = set()
+    for i in range(num):
+        corrupted.add(coords[i])
+    return corrupted
+
+def shortest_path(corrupted):
+    finish = (MAXX, MAXY)
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    visited_cost = defaultdict(lambda: math.inf)
+    min_pq = []
+    min_cost = math.inf
+    heapq.heappush(min_pq, (0, (0, 0)))
+
+    while min_pq:
+        cost, pos = heapq.heappop(min_pq)
+        x, y = pos
+
+        #Found shortest path
+        if pos == finish:
+            min_cost = cost
+            break
+        
+        for dx, dy in directions:
+            next_x, next_y = (x + dx, y + dy)
+            if 0 <= next_x <= MAXX and 0 <= next_y <= MAXY and cost + 1 < visited_cost[(next_x, next_y)] and (next_x, next_y) not in corrupted:
+                visited_cost[(next_x, next_y)] = cost + 1
+                heapq.heappush(min_pq, (cost + 1, (next_x, next_y)))
+
+    return min_cost
+    
+def draw_map(corrupted):
+    lines = []
+    for y in range(MAXY+1):
+        line = []
+        for x in range(MAXX+1):
+            if (x,y) in corrupted:
+                line.append('#')
+            else:
+                line.append('.')
+        lines.append("".join(line))
+    # os.system('cls' if os.name == 'nt' else 'clear')
+    print('\n'.join(lines))
+
+
+if __name__ == "__main__":
+    MAXX = MAXY = 70
+    coords = load_data('input')
+    i = 0
+    while True:
+        corrupted = simulate(i, coords)
+        if shortest_path(corrupted) == math.inf:
+            break
+        i += 1
+    print(coords[i-1])
