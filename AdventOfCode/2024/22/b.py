6ed71a3b736f3c1f6b8cc1347137ab7c4d496c98
Nick Yoder <nicholas.yoder@txse.com>
2025-08-01 09:57:02 -0700

    I was using counters improperly.

--- AdventOfCode/2024/22/b.py
+++ AdventOfCode/2024/22/b.py
@@ -1,53 +1,44 @@
 import time
-from collections import defaultdict
 from collections import Counter
 
-class Secret:
-    def __init__(self, secret_num):
-        self.secret = secret_num
-        self.price_flux = []
-        self.deltas_to_bananas = {}
+def mix(secret, x):
+    return secret ^ x
 
-    def mix(self, x):
-        self.secret = self.secret ^ x
+def prune(secret):
+    return secret % 16777216
 
-    def prune(self):
-        self.secret = self.secret % 16777216
-
-    def evolve(self, iterations):
-        for i in range(iterations):
-            prev_price = self.secret % 10
-            x = self.secret << 6
-            self.mix(x)
-            self.prune()
-            x = self.secret >> 5
-            self.mix(x)
-            self.prune()
-            x = self.secret * 2048
-            self.mix(x)
-            self.prune()
-            self.price_flux.append(self.secret % 10 - prev_price)
-            if i >= 3 and tuple(self.price_flux[-4:]) not in self.deltas_to_bananas:
-                self.deltas_to_bananas[tuple(self.price_flux[-4:])] = self.secret % 10
+def evolve(secret):
+    secret = prune(mix(secret, secret << 6))
+    secret = prune(mix(secret, secret >> 5))
+    secret = prune(mix(secret, secret * 2048))
+    return secret
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
 
-    master_dictionary = None
+    master_counter = Counter()
 
     with open('input') as file:
         lines = file.readlines()
         secrets = [int(line.strip()) for line in lines]
-        for num in secrets:
-            secret = Secret(num)
-            secret.evolve(2000)
-            if master_dictionary is None:
-                master_dictionary = secret.deltas_to_bananas
-            else:
-                master_dictionary = dict(Counter(master_dictionary) + Counter(secret.deltas_to_bananas))
+
+    for secret in secrets:
+        price_flux = []
+        deltas_to_bananas = {}
+        for _ in range(2000):
+            prev_secret = secret
+            secret = evolve(secret)
+            price_flux.append(secret % 10 - prev_secret % 10)
+            if len(price_flux) > 3 and tuple(price_flux[-4:]) not in deltas_to_bananas:
+                deltas_to_bananas[tuple(price_flux[-4:])] = secret % 10
+
+        if master_counter is None:
+            master_counter = deltas_to_bananas
+        else:
+            master_counter.update(deltas_to_bananas)
             
-    max_value = max(master_dictionary, key=master_dictionary.get)
-    print(f"{max_value}: {master_dictionary[max_value]}")
+    most_common = master_counter.most_common(1)[0]
+    print(f"{most_common[0]}: {most_common[1]}")
 
     end_time = time.perf_counter()
     print(f"Runtime: {end_time - start_time:.2f}s")
